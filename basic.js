d708e8760fb84456b3c58e9a8e487296775e34be
ayubhyd90 <106141478+ayubhyd90@users.noreply.github.com>
2022-07-06 16:10:41 +0530

    sk\data from old Html

---
+++ basic.js
@@ -0,0 +1,10 @@
+var a="abc"
+var b="18"
+var c=10
+var d="null"
+var e= "true"
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
---
+++ concde.js
@@ -0,0 +1,15 @@
+// var fname="john"
+// var iname="wick"
+// // console.log(john "+ "wick)
+
+// var num1=10
+// var num2=20
+// console.log (num1+num2)
+
+var a=10
+var b=10
+console.log(++a)
+
+// var num1=10
+// var num2=20
+// console.log (num1+num2)
